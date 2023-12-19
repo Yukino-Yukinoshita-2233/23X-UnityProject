07fa865ea4b82a718b38b656a6af5df0465c9255
Yukino-Yukinoshita-2233 <2410465558@qq.com>
2023-12-19 20:28:21 +0800

    新增Win界面,回到菜单按钮

--- Assets/Scripts/Item/Key/Win.cs
+++ Assets/Scripts/Item/Key/Win.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
 using Unity.Netcode;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Win : NetworkBehaviour
 {
     private new AudioSource audio;
-
+    [SerializeField] GameObject WinCanvas;
+    [SerializeField] Button ReturnMuneBtn;
     private void Start()
     {
         audio = GetComponent<AudioSource>();
+        WinCanvas = GameObject.Find("WinCanvas");
+        Debug.Log(WinCanvas.name);
+        ReturnMuneBtn = GameObject.Find("ReturnMuneBtn").GetComponent<Button>();
+        WinCanvas.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -16,7 +24,22 @@ public class Win : NetworkBehaviour
         {
             Debug.Log("IsServer: "+IsServer);
             Debug.Log("IsClient: "+IsClient);
+            StartCoroutine(GetReturmMuneBtnDowm());
         }
+    }
+
+    IEnumerator GetReturmMuneBtnDowm()
+    {
+        yield return null;
+
+        WinCanvas.SetActive(true);
+
+        ReturnMuneBtn.onClick.AddListener(ReturnMenuButtonClicked);
+    }
+
+    void ReturnMenuButtonClicked()
+    {
+        SceneManager.LoadScene("Start");
     }
 
     [ClientRpc]
